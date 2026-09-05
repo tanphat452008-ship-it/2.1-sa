c9b0da6c6bc5e46cb4ab6c99bfcf0b02e31095eb
tanphat452008-ship-it <tanphat452008@gmail.com>
2026-09-05 23:36:47 +0700

    Update CLoader.cpp

--- app/src/main/cpp/multiplayer/CLoader.cpp
+++ app/src/main/cpp/multiplayer/CLoader.cpp
@@ -8,64 +8,15 @@
 #include "crashlytics.h"
 #include "CSettings.h"
 #include "net/netgame.h"
-#include "java_systems/Speedometr.h"
-#include "java_systems/Donate.h"
-#include "java_systems/ObjectEditor.h"
-#include "java_systems/Styling.h"
-#include "java_systems/TireShop.h"
-#include "java_systems/casino/Dice.h"
-#include "java_systems/TheftAuto.h"
-#include "java_systems/casino/Baccarat.h"
+
+// Chỉ giữ lại các header khung cơ bản (HUD, Chat, Dialog, Tab)
+#include "java_systems/HUD.h"
 #include "java_systems/Tab.h"
-#include "java_systems/SelectEntity.h"
-#include "java_systems/AdminRecon.h"
-#include "java_systems/DailyReward.h"
-#include "java_systems/AutoShop.h"
-#include "java_systems/TechInspect.h"
-#include "java_systems/AucContainer.h"
-#include "java_systems/ChooseSpawn.h"
-#include "java_systems/casino/LuckyWheel.h"
-#include "java_systems/casino/Chip.h"
-#include "java_systems/Treasure.h"
-#include "gui/GiftNotify.h"
-#include "java_systems/FurnitureFactory.h"
-#include "java_systems/Samwill.h"
-#include "java_systems/GunStore.h"
-#include "java_systems/Achivments.h"
-#include "java_systems/OilFactory.h"
-#include "java_systems/FuelStation.h"
-#include "java_systems/RadialMenu.h"
-#include "java_systems/Authorization.h"
-#include "java_systems/ArmyGame.h"
-#include "java_systems/Medic.h"
-#include "java_systems/Inventory.h"
 #include "java_systems/GuiWrapper.h"
-#include "java_systems/mine/MineGame1.h"
-#include "java_systems/mine/MineGame2.h"
-#include "java_systems/mine/MineGame3.h"
-#include "java_systems/SkinShop.h"
-#include "java_systems/Registration.h"
-#include "java_systems/Taxi.h"
-#include "java_systems/Race.h"
-#include "java_systems/BattlePass.h"
-#include "java_systems/Milk.h"
-#include "GuiWrapper.h"
-#include "MagicStore.h"
-#include "SnapShotsWrapper.h"
-#include "Monologue.h"
-#include "MiningStore.h"
-#include "BuyPlate.h"
-#include "BusStation.h"
-#include "WarPoints.h"
-#include "BattlePassBuy.h"
 #include "JavaGui.h"
 
 void CLoader::loadBassLib()
 {
-   // LoadBassLibrary();
-   // BASS_Init(-1, 44100, BASS_DEVICE_MONO | BASS_DEVICE_3D);
-    //BASS_Set3DFactors(1, 0.15, 0);
-    //BASS_Apply3D();
 }
 
 void CLoader::initCrashLytics()
@@ -77,15 +28,11 @@ void CLoader::initCrashLytics()
     firebase::crashlytics::SetCustomKey("Nick", CSettings::m_Settings.szNickName);
 
     char str[100];
-
     sprintf(str, "0x%x", g_libGTASA);
     firebase::crashlytics::SetCustomKey("libGTASA.so", str);
 
     sprintf(str, "0x%x", g_libSAMP);
     firebase::crashlytics::SetCustomKey("libsamp.so", str);
-//
-//    sprintf(str, "0x%x", libc);
-//    firebase::crashlytics::SetCustomKey("libc.so", str);
 }
 
 void CLoader::loadSetting()
@@ -93,68 +40,37 @@ void CLoader::loadSetting()
     CSettings::LoadSettings(nullptr);
 }
 
+// BỌC AN TOÀN: Kiểm tra Exception và NULL để không bao giờ bị crash NewGlobalRef
 jclass LinkJavaClass(jclass localObj) {
     auto env = CJavaWrapper::GetEnv();
+    
+    // Nếu phía Java bị lỗi không tìm thấy Class (ClassNotFoundException) -> Xóa lỗi
+    if (env->ExceptionCheck()) {
+        env->ExceptionClear();
+    }
+
+    // Nếu con trỏ localObj bị NULL -> Trả về nullptr thay vì gọi NewGlobalRef gây crash
+    if (localObj == nullptr) {
+        return nullptr;
+    }
+
     auto globalRef = (jclass)env->NewGlobalRef(localObj);
     env->DeleteLocalRef(localObj);
     return globalRef;
 }
 
-
 void CLoader::initJavaClasses(JavaVM* pjvm) {
     JNIEnv* env = nullptr;
-    (void)pjvm->GetEnv((void**)&env, JNI_VERSION_1_6);
-
+    if (pjvm->GetEnv((void**)&env, JNI_VERSION_1_6) != JNI_OK) {
+        return;
+    }
     CJavaGui::clazz = LinkJavaClass(env->FindClass("com/game/russia/NewUiList"));
 
-    CBattlePassBuy::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/battle_pass/BattlePassBuy"));
-    CWarPoints::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/WarPoints"));
-    CBusStation::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/BusStation"));
-    CBattlePass::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/battle_pass/BattlePass"));
-    CBuyPlate::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/BuyPlate"));
-    CMiningStore::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/MiningStore"));
-    CMonologue::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/Monologue"));
-    SnapShotsWrapper::clazz = LinkJavaClass(env->FindClass("com/game/russia/EntitySnaps"));
-    CRace::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/Race"));
-    CMagicStore::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/magicStore/MagicStore"));
-    CTaxi::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/Taxi"));
-    CRegistration::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/Registration"));
-    CSkinShop::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/SkinAndAcsShop"));
-    CMineGame3::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/MineGame3"));
-    CMineGame2::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/MineGame2"));
-    CMineGame1::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/MineGame1"));
-    CInventory::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/inventory/Inventory"));
-    CMedic::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/PreDeath"));
-    CArmyGame::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/ArmyGame"));
-    CAuthorization::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/Authorization"));
-    CRadialMenu::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/RadialMenu"));
-    CFuelStation::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/FuelStation"));
-    COilFactory::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/fillingGames/OilFactory"));
-    CMilk::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/fillingGames/Milk"));
-    CAchivments::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/achivments/Achivments"));
-    CGunStore::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/GunShop"));
-    CSamwill::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/Samwill"));
-    CFurnitureFactory::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/FurnitureFactory"));
-    CGiftNotify::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/GiftNotify"));
-    CTreasure::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/treasure/Treasure"));
-    CChip::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/casino/BuySellChip"));
-    CLuckyWheel::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/casino/LuckyWheel"));
-    CChooseSpawn::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/ChooseSpawn"));
-    CAucContainer::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/AucContainer"));
-    CTechInspect::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/TechIspect"));
-    CAutoShop::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/AutoShop"));
-    CDailyReward::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/DailyReward"));
-    CAdminRecon::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/AdminRecon"));
+    // 2. Bảng Tab danh sách người chơi
     CTab::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/tab/Tab"));
-    CSpeedometr::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/Speedometer"));
-    CDonate::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/donate/Donate"));
-    CObjectEditor::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/AttachEdit"));
-    CStyling::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/styling/Styling"));
-    CTireShop::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/tire_shop/TireShop"));
-    CDice::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/casino/Dice"));
-    CTheftAuto::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/theft_auto/TheftAuto"));
-    CBaccarat::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/CasinoBaccarat"));
-    CActionsPed::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/ActionsPed"));
+
+    // 3. Hệ thống Chat / Đăng nhập cơ bản (nếu APK của bạn dùng các class này)
+    CAuthorization::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/Authorization"));
+    CRegistration::clazz = LinkJavaClass(env->FindClass("com/game/russia/gui/Registration"));
+
 }
-
-
